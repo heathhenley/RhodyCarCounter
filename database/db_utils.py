d3aa17e17f798db68d6bdf10185cf7802282d107
Heath Henley <heath.j.henley@gmail.com>
2023-02-12 23:50:13 -0500

    Pull out engine / env var nit

--- database/db_utils.py
+++ database/db_utils.py
@@ -7,23 +7,17 @@ import database.model as model
 import worker.utils
 
 
-def get_engine(db_connect_str=os.environ.get("DEV_DB_CONNECT_STR")):
+def get_engine(db_connect_str=os.environ.get("DB_CONNECT_STR")):
   return sqlalchemy.create_engine(db_connect_str, echo=False)
 
 # get list of all cameras in database
-def get_camera_list():
-  db_connect_str = os.environ.get("DEV_DB_CONNECT_STR")
-  engine = sqlalchemy.create_engine(db_connect_str, echo=True)
-  model.Base.metadata.create_all(bind=engine)
+def get_camera_list(engine):
   with sqlalchemy.orm.Session(engine) as session:
     stm = sqlalchemy.select(model.Camera)
     return list(session.scalars(stm))
 
 # stick all the cameras in the database
-def upsert_cameras_bulk(camera_list):
-  db_connect_str = os.environ.get("DEV_DB_CONNECT_STR")
-  engine = sqlalchemy.create_engine(db_connect_str, echo=True)
-  model.Base.metadata.create_all(bind=engine)
+def upsert_cameras_bulk(engine, camera_list):
   with sqlalchemy.orm.Session(engine) as session:
     for camera in camera_list:
       stm = (sqlalchemy.select(model.Camera)
@@ -50,14 +44,18 @@ def insert_data(camera_id, timestamp, image_name, vehicles, engine):
     session.commit()
 
 def main():
+  engine = get_engine()
+  model.Base.metadata.create_all(bind=engine)
 
   print("Upsert!")
   # insert cameras if not in db
-  upsert_cameras_bulk(utils.get_cams_from_page(utils.CAM_LIST_URL))
+  upsert_cameras_bulk(
+    engine,
+    worker.utils.get_cams_from_page(worker.utils.CAM_LIST_URL))
 
   print("Query them back!")
   # print them back out
-  print(get_camera_list())
+  print(get_camera_list(engine))
 
 
 if __name__ == "__main__":
