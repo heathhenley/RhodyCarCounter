ae062684062b6e77db496645e234f1fec5020b3e
Heath Henley <heath.j.henley@gmail.com>
2023-02-24 13:37:16 -0500

    Chart formatting tweaks

--- frontend/traffic_count/src/components/TrafficChart.js
+++ frontend/traffic_count/src/components/TrafficChart.js
@@ -1,10 +1,14 @@
-import { CartesianGrid, LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { CartesianGrid, BarChart, Line, XAxis, YAxis, Tooltip, Bar,
+         ResponsiveContainer } from 'recharts';
+
+
+const formatTimestamps = (timestamp_in_seconds) => {
+  let date = new Date(timestamp_in_seconds * 1000.0);
+  return date.toLocaleTimeString("en-US", {timeZone: "America/New_York"});
+}
 
 function TrafficChart(props) {
-  const formatTimestamps = (timestamp_in_seconds) => {
-    let date = new Date(timestamp_in_seconds * 1000.0);
-    return date.toLocaleTimeString("en-US", {timeZone: "America/New_York"});
-  }
+
   let data = props.data;
   data.forEach(x => {
     x.timestamp_in_seconds = Date.parse(x.timestamp + "Z") / 1000.0;
@@ -12,16 +16,19 @@ function TrafficChart(props) {
   
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data}>
-        <Line type="monotone" stroke="#8884d8" dataKey="vehicles" />
+      <BarChart data={data}>
+        <Bar stroke="#8884d8" dataKey="vehicles" />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis
           dataKey="timestamp_in_seconds"
           type="number"
           domain={['auto', 'auto']}
           tickFormatter={formatTimestamps}/>
-        <Tooltip labelFormatter={formatTimestamps}/>
-      </LineChart>
+        <YAxis label={{ value: "Vehicles Detected in Frame", angle: -90 }}/>
+        <Tooltip
+          formatter={(value) => [value, "Vehicles"]}
+          labelFormatter={formatTimestamps}/>
+      </BarChart>
     </ResponsiveContainer>
   );
 }
