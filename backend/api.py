afac928413a5924256a6a5d244ccf99b2b674780
Heath Henley <heath.j.henley@gmail.com>
2023-03-03 23:48:14 -0500

    Fix cors config?

--- backend/api.py
+++ backend/api.py
@@ -42,7 +42,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["GET"],
     allow_headers=["*"],
 )
