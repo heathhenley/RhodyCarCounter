35fd67e8f811b4d2df32d4db9e24d77ce55a89a5
Heath Henley <heath.j.henley@gmail.com>
2023-03-05 10:22:08 -0500

    Add some links

--- frontend/traffic_count/src/components/PageHeader.js
+++ frontend/traffic_count/src/components/PageHeader.js
@@ -5,7 +5,7 @@ const PageHeader = () => {
     <header className="App-header">
       <div className="p-5 d-flex flex-column justify-content-center align-items-center">
         <h1>Traffic Cams in Providence, RI</h1>
-        <h4>Streams from RIDOT - processed using YOLO </h4>
+        <h4>Streams from <a href="https://www.dot.ri.gov/travel/index.php" >RIDOT</a> - processed using <a href="https://arxiv.org/pdf/1612.08242v1.pdf">YOLOv2</a></h4>
       </div>
       <NavBarTraffic />
     </header>
