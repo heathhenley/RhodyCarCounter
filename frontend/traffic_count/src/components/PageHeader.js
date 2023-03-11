d79ba9ba2d963835beeca7b5b5bdbb20a87da4d3
Heath Henley <heath.j.henley@gmail.com>
2023-03-11 00:38:50 -0500

    Update model

--- frontend/traffic_count/src/components/PageHeader.js
+++ frontend/traffic_count/src/components/PageHeader.js
@@ -7,7 +7,7 @@ const PageHeader = () => {
       style={{ backgroundColor: 'white'}}>
       <div className="p-4 d-flex flex-column justify-content-center align-items-center">
         <h1>Traffic Cams in Providence, RI</h1>
-        <h4>Streams from <a href="https://www.dot.ri.gov/travel/index.php" >RIDOT</a> - processed using <a href="https://arxiv.org/pdf/1612.08242v1.pdf">YOLOv2</a></h4>
+        <h4>Streams from <a href="https://www.dot.ri.gov/travel/index.php" >RIDOT</a> - processed using <a href="https://docs.ultralytics.com/">YOLOv8</a></h4>
       </div>
       <NavBarTraffic />
     </header>
