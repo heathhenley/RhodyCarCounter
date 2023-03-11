18287bc36c0f8b7e16e3484bfeef5f94286ec55c
Heath Henley <heath.j.henley@gmail.com>
2023-03-11 15:26:05 -0500

    More copy updates

--- frontend/traffic_count/src/components/PageHeader.js
+++ frontend/traffic_count/src/components/PageHeader.js
@@ -6,7 +6,7 @@ const PageHeader = () => {
       className="App-header shadow rounded mt-3"
       style={{ backgroundColor: 'white'}}>
       <div className="p-4 d-flex flex-column justify-content-center align-items-center">
-        <h1>Detecting Cars in RI Traffic Cams</h1>
+        <h1>Detecting Cars in RI Traffic Feeds</h1>
         <h4>Streams from <a href="https://www.dot.ri.gov/travel/index.php" >RIDOT</a> - processed using <a href="https://docs.ultralytics.com/">YOLOv8</a></h4>
       </div>
       <NavBarTraffic />
