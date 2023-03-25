1720a083d03cd57f39f1df1ed11e5d48517ce7e7
Heath Henley <heath.j.henley@gmail.com>
2023-03-25 12:38:41 -0400

    First shot at running the full app with docker compose

---
+++ backend/worker/copy_data_dev_db.py
@@ -0,0 +1,31 @@
+import requests
+import sqlalchemy
+import database.db_utils
+import database.model as model
+
+API = "https://rhodycarcounter-production.up.railway.app/api/cameras/"
+
+def main():
+  camera_list = requests.get(API).json()
+  engine = database.db_utils.get_engine()
+  with sqlalchemy.orm.Session(engine) as session:
+    for camera in camera_list:
+      stm = (sqlalchemy.select(model.Camera)
+        .where(model.Camera.id_from_website == camera["id_from_website"]))
+      if session.scalars(stm).one_or_none():
+        continue
+      db_camera = model.Camera(
+        id_from_website=camera["id_from_website"],
+        description=camera["description"],
+        name=camera["name"],
+        url=camera["url"],
+        latitude=camera["latitude"],
+        longitude=camera["longitude"]
+      )
+      session.add(db_camera)
+    session.commit()
+
+  pass
+
+if __name__ == "__main__":
+  main()
